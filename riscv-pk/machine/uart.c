94e8143a1d0233bda8248843d8bb3ce13727ed1d
maoyuchaxue <wangjt15@mails.tsinghua.edu.cn>
2018-11-29 14:44:53 +0800

    convert uart protocol in riscv-pk to uartlite

--- riscv-pk/machine/uart.c
+++ riscv-pk/machine/uart.c
@@ -6,26 +6,34 @@ volatile uint32_t* uart;
 
 void uart_putchar(uint8_t ch)
 {
-#ifdef __riscv_atomic
-    int32_t r;
-    do {
-      __asm__ __volatile__ (
-        "amoor.w %0, %2, %1\n"
-        : "=r" (r), "+A" (uart[UART_REG_TXFIFO])
-        : "r" (ch));
-    } while (r < 0);
-#else
-    volatile uint32_t *tx = uart + UART_REG_TXFIFO;
-    while ((int32_t)(*tx) < 0);
-    *tx = ch;
-#endif
+// #ifdef __riscv_atomic
+//     int32_t r;
+//     do {
+//       __asm__ __volatile__ (
+//         "amoor.w %0, %2, %1\n"
+//         : "=r" (r), "+A" (uart[UART_REG_TXFIFO])
+//         : "r" (ch));
+//     } while (r < 0);
+// #else
+//     volatile uint32_t *tx = uart + UART_REG_TXFIFO;
+//     while ((int32_t)(*tx) < 0);
+//     *tx = ch;
+// #endif
+  int32_t stat = uart[UART_REG_STAT];
+  while (stat & UART_STAT_TXFULL) {
+    stat = uart[UART_REG_STAT];
+  }
+  uart[UART_REG_TXFIFO] = ch;
 }
 
 int uart_getchar()
 {
-  int32_t ch = uart[UART_REG_RXFIFO];
-  if (ch < 0) return -1;
-  return ch;
+  int32_t stat = uart[UART_REG_STAT];
+  if (stat & UART_STAT_RXVALID) {
+    return uart[UART_REG_RXFIFO];
+  } else {
+    return -1;
+  }
 }
 
 struct uart_scan
@@ -53,12 +61,12 @@ static void uart_prop(const struct fdt_scan_prop *prop, void *extra)
 static void uart_done(const struct fdt_scan_node *node, void *extra)
 {
   struct uart_scan *scan = (struct uart_scan *)extra;
-  if (!scan->compat || !scan->reg || uart) return;
+  // if (!scan->compat || !scan->reg || uart) return;
 
   // Enable Rx/Tx channels
-  uart = (void*)(uintptr_t)scan->reg;
-  uart[UART_REG_TXCTRL] = UART_TXEN;
-  uart[UART_REG_RXCTRL] = UART_RXEN;
+  uart = (void*)(uintptr_t)0x60000000;
+  uart[UART_REG_CTRL] = UART_CTRL_TXRST | UART_CTRL_RXRST;
+  uart[UART_REG_CTRL] = UART_CTRL_IE;
 }
 
 void query_uart(uintptr_t fdt)
--- riscv-pk/machine/uart.h
+++ riscv-pk/machine/uart.h
@@ -5,14 +5,25 @@
 
 extern volatile uint32_t* uart;
 
-#define UART_REG_TXFIFO		0
-#define UART_REG_RXFIFO		1
+#define UART_REG_RXFIFO		0
+#define UART_REG_TXFIFO		1
+#define UART_REG_STAT		2
+#define UART_REG_CTRL		3
 #define UART_REG_TXCTRL		2
 #define UART_REG_RXCTRL		3
 #define UART_REG_DIV		4
 
 #define UART_TXEN		 0x1
 #define UART_RXEN		 0x1
+
+#define UART_STAT_TXFULL  0x8
+#define UART_STAT_TXEMPTY 0x4
+#define UART_STAT_RXFULL  0x2
+#define UART_STAT_RXVALID 0x1
+
+#define UART_CTRL_IE	  0x10
+#define UART_CTRL_TXRST	  0x2
+#define UART_CTRL_RXRST	  0x1
 
 void uart_putchar(uint8_t ch);
 int uart_getchar();
